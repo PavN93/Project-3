55e82c386230efa4c1260787099a04b0aa31f690
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-04 23:01:16 +0000

    Adding initial homepage

---
+++ client/src/components/Search/search.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./search.css";
+
+const Search = () => {
+  const [fetchResults, setFetchResults] = useState([]);
+  const [search, setSearch] = useState();
+
+  function getSearchResults() {
+    console.log("Searching for:", search);
+    const searchedPlant = fetchResults.filter(
+      (fetchResults) => search.indexOf(fetchResults.name) > -1
+    );
+    console.log(searchedPlant);
+    setFetchResults(searchedPlant);
+  }
+
+  function handleInputChange(event) {
+    setSearch(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    getSearchResults();
+  }
+
+  return (
+    <div className="container">
+      <form className="form-inline">
+      <i className="fas fa-search"></i>
+        <input 
+          value={search}
+          onChange={handleInputChange}
+          className="input"
+          type="search"
+          placeholder="Search plants"
+        />
+        <button
+          onClick={handleFormSubmit}
+          className="btn btn-light"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
