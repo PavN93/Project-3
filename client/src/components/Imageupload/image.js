675d310c7c74e1ce7a7d75565f78ef0a7bece475
Shivey Singh <shivey_khangura@hotmail.com>
2021-03-08 16:28:30 +0000

    added image upload component

---
+++ client/src/components/Imageupload/image.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './image.css';
+
+
+
+class ImageUpload extends React.Component{
+  state = {
+    imageUrl: null,
+    imageAlt: null,
+  }
+
+  render() {
+    const { imageUrl, imageAlt } = this.state;
+
+    return (
+      <main className="App">
+        <section className="left-side">
+          <form>
+            <div className="form-group">
+              <input type="file"/>
+            </div>
+
+            <button type="button" className="btn" onClick={this.handleImageUpload}>Submit</button>
+            <button type="button" className="btn widget-btn">Upload Via Widget</button>
+          </form>
+        </section>
+        <section className="right-side">
+          <p>The resulting image will be displayed here</p>
+          {imageUrl && (
+            <img src={imageUrl} alt={imageAlt} className="displayed-image"/>
+          )}
+        </section>
+      </main>
+    );
+  }
+}
+
+export default ImageUpload;
+  
