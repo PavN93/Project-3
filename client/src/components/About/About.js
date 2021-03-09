c1d630e67f20a4ad82d86d12f5f16ced920ed327
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-09 13:59:16 +0000

    adding sign up page and formatting login form

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,13 +1,16 @@
 import React from "react";
-import "./About.css"
+import "./About.css";
 
 const About = () => {
   return (
     <section className="container">
       <h1>Plantica</h1>
       <h4>
-        Welcome to Plantica. An application for botanist and enthusiasts to
-        research and save plants.
+        From botanists to ecologists, hikers to hunters, the world is filled with likeminded
+        nature enthusiasts wanting to record what we find.
+        
+        Plantica is a tool to help users identify and record plant species, as
+        well as offer information on different flora.
       </h4>
     </section>
   );
--- client/src/routes/Routed.js
+++ client/src/routes/Routed.js
@@ -1,10 +1,14 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from "../pages/Homepage/Homepage";
+import Login from "../pages/LoginPage/LoginPage";
+import SignUp from "../pages/SignUpPage/SignUpPage";
 
 const Routed = () => {
   return (
     <Router basename='/'>
       <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/Sign-up' component={SignUp}/>
     </Router>
   )
 }
