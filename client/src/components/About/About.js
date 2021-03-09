b8f6df8929c19f3f3162ccbbb127f251254fea20
Pawel <paweln993@gmail.com>
2021-03-09 15:21:03 +0000

    file names to uppercase

---
+++ client/src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css"
+
+const About = () => {
+  return (
+    <section className="container">
+      <h1>Plantica</h1>
+      <h4>
+        Welcome to Plantica - an application for botanist and enthusiasts to
+        research and save plants.
+      </h4>
+    </section>
+  );
+};
+
+export default About;
