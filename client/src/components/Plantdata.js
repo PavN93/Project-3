ead31edf7cb7dbf2f221d38ec78450b9a09871d8
Shivey Singh <shivey_khangura@hotmail.com>
2021-03-10 18:25:16 +0000

    added search parameter

--- client/src/components/Plantdata.js
+++ client/src/components/Plantdata.js
@@ -10,10 +10,10 @@ const PlantData = () => {
     console.log("intial plant data render");
     const queryURL = "https://trefle.io/api/v1/plants/search?token=ygxSP6ZBnAfDFaBTRKTtVkg7G56ajDSjvz5LkVnjHfw&q={search}";
     const fetchPlants = async () => {
-      const response = await fetch(queryURL);
-      const payload = await response.json();
-      console.log("response data", payload);
-      setPlants(payload.results || []); // if search fails fallbacks or empty array
+    const response = await fetch(queryURL);
+    const payload = await response.json();
+    console.log("response data", payload);
+    setPlants(payload.results || []); // if search fails fallbacks or empty array
     };
     fetchPlants();
   }, [])};
