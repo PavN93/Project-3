15d484cbd7c3da9fcf8f964cda6ffb0a69826b84
Shivey Singh <shivey_khangura@hotmail.com>
2021-03-14 18:17:05 +0000

    deleted util file not needed

--- client/src/components/usePlantdata.js
+++ client/src/components/usePlantdata.js
@@ -1,24 +1,25 @@
-import { useState, useEffect, } from "react";
+//import { useState, useEffect, } from "react";
 
 
-const usePlantData = () => {
 
-  const [plants, setPlants] = useState([]);
-  const [search, setSearch] = useState('');
+const PlantData = () => {
+
+  //const [plants, setPlants] = useState([]);
+  //const [search, setSearch] = useState('');
 
   // fetch data and render on page load once
-  useEffect(() => {
-    console.log(search)
-    if (search) {
+  //useEffect(() => {
+   // console.log(search)
+    //if (search) {
       try {
         
         console.log("intial plant data render");
-        const queryURL = `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=ygxSP6ZBnAfDFaBTRKTtVkg7G56ajDSjvz5LkVnjHfw&q=${search}`;
+        const queryURL = `/api/getplants`;
         const fetchPlants = async () => {
           const response = await fetch(queryURL);
           const payload = await response.json();
           console.log("response data", payload);
-          setPlants(payload.results || []); // if search fails fallbacks or empty array
+          //setPlants(payload.results || []); // if search fails fallbacks or empty array
         };
         fetchPlants();
       }
@@ -26,12 +27,10 @@ const usePlantData = () => {
         console.log("apicall error", error);
         
       }
-    }
-  }, [search])
-  return {plants,setSearch,search}
+    //}
+  /*}, [search])
+  return {plants,setSearch,search}*/
 };
 
 
-
-
-export default usePlantData;
+export default PlantData;
