761cf10b029c157095d333af2b6bd04651222f4a
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-16 23:30:42 +0000

    React slick carousel added

--- client/src/components/Collection/Collection.js
+++ client/src/components/Collection/Collection.js
@@ -1,37 +1,51 @@
 import React, { useEffect, useContext } from "react";
 import "./Collection.css";
 import PlantResultsContext from "../../context/PlantData";
+import Slider from "react-slick";
 
 const PlantCollection = () => {
-
   const { plants } = useContext(PlantResultsContext);
 
   useEffect(() => {
     console.log("Plant array", plants);
-   }, [plants]);
+  }, [plants]);
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+  };
 
   return (
     <div className="plantData">
       <h2>Sample plant data</h2>
       <div className="ui olive cards">
-        {plants.map((result) => (
-          <div className="ui card">
-            <div className="content">
-              <img
-                src={result.image_url}
-                className="ui image"
-                alt={result.common_name}
-              />
-              <div className="header">{result.common_name}</div>
-              <div className="meta">First founded: {result.year}</div>
-              <div className="description">Scientific name: {result.scientific_name}</div>
-              <div className="description">{result.observations}</div>
+        <Slider {...settings}>
+          {plants.map((result) => (
+            <div className="ui card">
+              <div className="content">
+                <img
+                  src={result.image_url}
+                  className="ui image plantImage"
+                  alt={result.common_name}
+                />
+                <div className="header">{result.common_name}</div>
+                <div className="meta">First founded: {result.year}</div>
+                <div className="description">
+                  Scientific name: {result.scientific_name}
+                </div>
+                <div className="description">{result.observations}</div>
+              </div>
+              <div className="extra content">
+                <button className="ui olive basic button">View more</button>
+              </div>
             </div>
-            <div className="extra content">
-              <button className="ui olive basic button">View more</button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </div>
   );
