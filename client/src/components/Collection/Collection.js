f569ba344ad3315022075008fc0713e9959803b8
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-16 22:45:24 +0000

    Plant data now displaying on homepage

--- client/src/components/Collection/Collection.js
+++ client/src/components/Collection/Collection.js
@@ -1,62 +1,37 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import "./Collection.css";
+import PlantResultsContext from "../../context/PlantData";
 
 const PlantCollection = () => {
-  // This will map over the trefle API to pull out sample plant data
-  // The component can be reused throughout the app
+
+  const { plants } = useContext(PlantResultsContext);
+
+  useEffect(() => {
+    console.log("Plant array", plants);
+   }, [plants]);
 
   return (
     <div className="plantData">
       <h2>Sample plant data</h2>
       <div className="ui olive cards">
-        <div className="ui card">
-          <div className="content">
-            <img
-              src="https://react.semantic-ui.com/images/wireframe/image.png"
-              className="ui image" alt="placeholder"
-            />
-            <div className="header">Plant name</div>
-            <div className="meta">Scientific name</div>
-            <div className="description">Description</div>
+        {plants.map((result) => (
+          <div className="ui card">
+            <div className="content">
+              <img
+                src={result.image_url}
+                className="ui image"
+                alt={result.common_name}
+              />
+              <div className="header">{result.common_name}</div>
+              <div className="meta">First founded: {result.year}</div>
+              <div className="description">Scientific name: {result.scientific_name}</div>
+              <div className="description">{result.observations}</div>
+            </div>
+            <div className="extra content">
+              <button className="ui olive basic button">View more</button>
+            </div>
           </div>
-          <div className="extra content"><button className="ui olive basic button">View more</button></div>
-        </div>
-        <div className="ui card">
-          <div className="content">
-            <img
-              src="https://react.semantic-ui.com/images/wireframe/image.png"
-              class="ui image" alt="placeholder"
-            />
-            <div className="header">Plant name</div>
-            <div className="meta">Scientific name</div>
-            <div className="description">Description</div>
-          </div>
-          <div className="extra content"><button className="ui olive basic button">View more</button></div>
-        </div>
-        <div className="ui card">
-          <div className="content">
-            <img
-              src="https://react.semantic-ui.com/images/wireframe/image.png"
-              className="ui image" alt="placeholder"
-            />
-            <div className="header">Plant name</div>
-            <div className="meta">Scientific name</div>
-            <div className="description">Description</div>
-          </div>
-          <div className="extra content"><button className="ui olive basic button">View more</button></div>
-        </div>
-        <div className="ui card">
-          <div className="content">
-            <img
-              src="https://react.semantic-ui.com/images/wireframe/image.png"
-              class="ui image" alt="placeholder"
-            />
-            <div className="header">Plant name</div>
-            <div className="meta">Scientific name</div>
-            <div className="description">Description</div>
-          </div>
-          <div className="extra content"><button className="ui olive basic button">View more</button></div>
-        </div>
+        ))}
       </div>
     </div>
   );
