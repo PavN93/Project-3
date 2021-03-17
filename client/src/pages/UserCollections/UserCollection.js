dc68483516cf6571b63b9e8a320e031c41940638
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-17 21:52:26 +0000

    updating user profile page

---
+++ client/src/pages/UserCollections/UserCollection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Menu from "../../components/Menu/Menu";
+;
+import Footer from "../../components/Footer/Footer";
+
+function UserCollection() {
+  
+  return (
+    <div>
+      <Menu />
+      {/* <Collections /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default UserCollection;
