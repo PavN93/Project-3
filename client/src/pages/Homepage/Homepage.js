a2e74a82438a57767c32c9957681ee51319876e3
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-18 21:14:59 +0000

    carousel hidden on load

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -3,7 +3,6 @@ import Menu from "../../components/Menu/Menu";
 import Weather from "../../components/Weather/Weather"
 import Banner from "../../components/Banner/Banner";
 import Search from "../../components/Search/Search";
-import SearchPlantData from "../../components/SearchPlantData/SearchPlantData";
 import Footer from "../../components/Footer/Footer";
 
 // components to be added in here are:
@@ -18,7 +17,6 @@ function Home() {
       <Banner/>
       <Weather/>
       <Search/>
-      <SearchPlantData />
       <Footer/>
     </div>
   );
