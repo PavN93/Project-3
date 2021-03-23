c1bc027ed38ae47740ddf8d218ce712bb4c1542d
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-03-23 23:57:47 +0000

    refactored for assistance

--- client/src/components/PlantOfTheDay/PlantOfTheDay.js
+++ client/src/components/PlantOfTheDay/PlantOfTheDay.js
@@ -1,11 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./PlantOfTheDay.css";
-import moment from 'moment';
+import moment from "moment";
 import { motion } from "framer-motion";
 
 const DailyPlant = () => {
   const [dailyPlant, setDailyPlant] = useState("");
-  const [plantReveal, setPlantReveal] = useState(false);
+  const [plantReveal, setPlantReveal] = useState(true);
 
   function revealRandomPlant() {
     const plantURL = `/api/getRandomPlant/`;
@@ -19,7 +19,7 @@ const DailyPlant = () => {
       onBtnClick();
 
       localStorage.setItem("Date", new Date().toString());
-      localStorage.setItem("Plant URL", JSON.stringify(randomPlant));
+      localStorage.setItem("PlantURL", JSON.stringify(randomPlant));
     };
     fetchPlants();
   }
@@ -32,12 +32,15 @@ const DailyPlant = () => {
 
   useEffect(() => {
     if (localStorage.getItem("Date") === null) {
-      setPlantReveal(false)
+      setPlantReveal(false);
     } else if (
-      localStorage.getItem("Date") === moment().format('ddd MMM Do YYYY') && localStorage.getItem("Plant URL" !== null) ) {
-      setPlantReveal(true) }
-  }, [])
-  console.log(plantReveal);
+      localStorage.getItem("Date") === moment().format("ddd MMM Do YYYY") &&
+      localStorage.getItem("PlantURL") != null)
+     {
+      console.log("test")
+      setPlantReveal(true);
+     }
+  }, []);
 
   if (plantReveal === false) {
     return (
@@ -69,59 +72,59 @@ const DailyPlant = () => {
         </div>
       </div>
     );
-  }
-
-  else if (plantReveal === true) {
-  return (
-    <div className="ui container plantOfTheDay">
-      <div className="ui stackable two column grid">
-        <div className="row">
-          <div className="column">
-            <div className="plantReveal">
-              <h1>Plant of the day</h1>
-              <motion.button disable
-                whileTap={{ scale: 0.9 }}
-                className="ui button revealBtn"
-                ref={btnRef}
-              >
-                Click to reveal
-              </motion.button>
-            </div>
-          </div>
-
-          <motion.div
-            animate={{ rotate: 360 }}
-            transition={{
-              type: "spring",
-              damping: 10,
-              mass: 0.75,
-              stiffness: 100,
-              duration: 3,
-            }}
-            className="column plantCard"
-          >
-            <div className="ui card plantOfTheDay">
-              <div className="content">
-                <img
-                  src={dailyPlant.image_url}
-                  className="ui image plantImage"
-                  alt="plant"
-                />
-                <div className="header">{dailyPlant.common_name}</div>
-                <div className="meta">First founded: {dailyPlant.year}</div>
-                <div className="description">
-                  Scientific name: {dailyPlant.scientific_name}
-                </div>
-                <div className="description">
-                  Family: {dailyPlant.family_common_name}
-                </div>
+  } else if (plantReveal === true) {
+    return (
+      <div className="ui container plantOfTheDay">
+        <div className="ui stackable two column grid">
+          <div className="row">
+            <div className="column">
+              <div className="plantReveal">
+                <h1>Plant of the day</h1>
+                <motion.button
+                  disable
+                  whileTap={{ scale: 0.9 }}
+                  className="ui button revealBtn"
+                  ref={btnRef}
+                >
+                  Click to reveal
+                </motion.button>
               </div>
             </div>
-          </motion.div>
+
+            <motion.div
+              animate={{ rotate: 360 }}
+              transition={{
+                type: "spring",
+                damping: 10,
+                mass: 0.75,
+                stiffness: 100,
+                duration: 3,
+              }}
+              className="column plantCard"
+            >
+              <div className="ui card plantOfTheDay">
+                <div className="content">
+                  <img
+                    src={dailyPlant.image_url}
+                    className="ui image plantImage"
+                    alt="plant"
+                  />
+                  <div className="header">{dailyPlant.common_name}</div>
+                  <div className="meta">First founded: {dailyPlant.year}</div>
+                  <div className="description">
+                    Scientific name: {dailyPlant.scientific_name}
+                  </div>
+                  <div className="description">
+                    Family: {dailyPlant.family_common_name}
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}};
+    );
+  }
+};
 
 export default DailyPlant;
